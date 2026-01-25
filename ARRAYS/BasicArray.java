ddf99a8c1526d01443be21049ed0d69e91c7f237
KaustubhPatil3 <kaustubhpatil.sknsits.comp>
2026-01-25 16:06:04 +0530

    sum of two added

--- ARRAYS/BasicArray.java
+++ ARRAYS/BasicArray.java
@@ -1,15 +1,14 @@
 package ARRAYS;
 public class BasicArray {
+
     public static void main(String[] args) {
 
         int[] arr = {10, 20, 30, 40, 50};
 
-        // Print array
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
 
-        // Update
         arr[2] = 100;
 
         System.out.println("\nAfter update:");
